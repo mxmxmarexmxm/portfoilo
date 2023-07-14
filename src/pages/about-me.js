fb722a0f5f286f9a8abafc1dba699a1a05a56809
marinko malencic <marinko.malencic542@gmail.com>
2023-07-14 14:14:16 +0200

    chore: create about me starter

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -1,5 +1,13 @@
-function Home() {
-  return <div></div>;
+function AboutMe() {
+  return (
+    <div className="flex items-center min-h-screen px-16">
+      <h1 className="text-5xl font-black text-white">
+        Hi, I'm Marinko
+        <br />
+        Frontend Developer
+      </h1>
+    </div>
+  );
 }
 
-export default Home;
+export default AboutMe;
