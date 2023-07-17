c66d52b8c170c2f957bdd0c5967427fd655a0145
marinko malencic <marinko.malencic542@gmail.com>
2023-07-17 20:21:30 +0200

    feat: create project data file

---
+++ src/data/projects-data.js
@@ -0,0 +1,16 @@
+import maskeDesk from '../assets/img/maskeDesk.png';
+import maskePhone from '../assets/img/maskePhone.png';
+
+const projects = [
+  {
+    title: 'E-COMMERCE APPLICATION',
+    name: 'Maske.rs',
+    phoneImgUrl: maskePhone,
+    desktopImgUrl: maskeDesk,
+    description:
+      'Online store for selling mobile devices equipment, especially for covers.',
+    url: 'https://maske.rs/',
+  },
+];
+
+export default projects;
