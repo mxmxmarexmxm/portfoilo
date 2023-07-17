47405befbc352ffd2e3e3486a052b51b538bfdb0
marinko malencic <marinko.malencic542@gmail.com>
2023-07-17 19:49:40 +0200

    style: restyled layout

--- src/components/ui/Layout.js
+++ src/components/ui/Layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Layout = (props) => {
   return (
-    <div className="w-full min-h-screen bg-black ">
+    <div className="w-full min-h-screen bg-black text-white p-4">
       {props.children}
     </div>
   );
