03a50772953a2e148533498e9dada89caae85363
marinko malencic <marinko.malencic542@gmail.com>
2023-07-18 18:18:41 +0200

    feat: add logo badge container

---
+++ src/components/ui/logo-badge-container.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import react from './../../assets/img/react.png';
+import next from './../../assets/img/next.png';
+import tailwind from './../../assets/img/tailwind.png';
+import graphql from './../../assets/img/graphql.png';
+
+const logos = {
+  react: react,
+  next: next,
+  tailwind: tailwind,
+  graphql: graphql,
+};
+
+const LogoBadgeContainer = (props) => {
+  const { techs } = props;
+  return (
+    <div className="w-full flex justify-center items-center gap-4">
+      {techs.map((tech) => (
+        <div className="h-12 sm:h-20  flex aspect-square rounded-full hover:scale-125">
+          <img
+            src={logos[tech]}
+            className="w-full object-contain"
+            alt={`${tech}-logo`}
+          />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default LogoBadgeContainer;
