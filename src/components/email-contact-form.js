3c519865d27b610982e4391d78a1711e96922d83
marinko malencic <marinko.malencic542@gmail.com>
2023-07-20 22:38:16 +0200

    style: restyled contact form

--- src/components/email-contact-form.js
+++ src/components/email-contact-form.js
@@ -5,8 +5,8 @@ const EmailContactForm = () => {
   const form = useRef();
 
   const sendEmail = (e) => {
-    e.preventDefault(); 
-    
+    e.preventDefault();
+
     emailjs
       .sendForm(
         'service_qtpzty8',
@@ -25,14 +25,30 @@ const EmailContactForm = () => {
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail} className='text-white'>
-      <label>Name</label>
-      <input type="text" name="from_name" className='text-black' />
-      <label>Email</label>
-      <input type="email" name="user_email" className='text-black' />
-      <label>Message</label>
-      <textarea name="message" />
-      <input type="submit" value="Send" className='text-white' />
+    <form
+      ref={form}
+      onSubmit={sendEmail}
+      className="text-white bg-gray-700 flex flex-col p-16 w-1/2 m-auto gap-4 mb-8 rounded-xl border-2 border-gray-600"
+    >
+      <label className="font-bold text-xl">Name</label>
+      <input
+        type="text"
+        name="from_name"
+        className="text-black p-4 rounded-lg"
+      />
+      <label className="font-bold text-xl">Email</label>
+      <input
+        type="email"
+        name="user_email"
+        className="text-black p-4 rounded-lg"
+      />
+      <label className="font-bold text-xl">Message</label>
+      <textarea name="message" className="text-black h-24 rounded-lg p-4" />
+      <input
+        type="submit"
+        value="Send"
+        className="text-white p-4 mt-4 text-xl rounded-lg bg-zinc-900 font-bold cursor-pointer border-4 border-gray-600"
+      />
     </form>
   );
 };
