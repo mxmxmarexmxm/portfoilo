40d93d7e6ca4c3b734f29e2cbb2a5fa06c1aa317
marinko malencic <marinko.malencic542@gmail.com>
2023-07-24 20:06:38 +0200

    style: fixed unnecessary gap

--- src/components/ProjectCarrousel.js
+++ src/components/ProjectCarrousel.js
@@ -27,7 +27,7 @@ const ProjectCarrousel = () => {
   };
 
   return (
-    <>
+    <div className="flex flex-col">
       <div className="flex items-center justify-between sm:px-4">
         <button onClick={() => carrouselHandler('backward')}>
           <Backward className="  cursor-pointer w-8 sm:w-24" />
@@ -40,14 +40,14 @@ const ProjectCarrousel = () => {
         </button>
       </div>
 
-      <div className="bg-gray-500 w-64 h-8 self-center rounded-2xl flex justify-start overflow-hidden border-2 border-gray-700">
+      <div className="bg-gray-500 w-64 h-8 self-center rounded-2xl flex justify-start overflow-hidden border-2 border-gray-700 mt-8">
         <div
           className={`w-[${
             ((project + 1) / projectLength) * 100
           }%] bg-gray-600 h-full border-r-2 border-gray-700`}
         ></div>
       </div>
-    </>
+    </div>
   );
 };
 
