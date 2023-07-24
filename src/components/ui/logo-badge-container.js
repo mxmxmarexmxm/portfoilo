60440d9c9054e0010143496cfecf42ec119aef5c
marinko malencic <marinko.malencic542@gmail.com>
2023-07-24 19:06:31 +0200

    chore: update logos array with new imgs

--- src/components/ui/logo-badge-container.js
+++ src/components/ui/logo-badge-container.js
@@ -3,12 +3,20 @@ import react from './../../assets/img/react.png';
 import next from './../../assets/img/next.png';
 import tailwind from './../../assets/img/tailwind.png';
 import graphql from './../../assets/img/graphql.png';
+import firebase from './../../assets/img/firebase.png';
+import css from './../../assets/img/css.png';
+import html from './../../assets/img/html.png';
+import js from './../../assets/img/js.png';
 
 const logos = {
   react: react,
   next: next,
   tailwind: tailwind,
   graphql: graphql,
+  firebase: firebase,
+  css: css,
+  html: html,
+  js: js,
 };
 
 const LogoBadgeContainer = (props) => {
