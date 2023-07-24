4c3ddb2c21a9a9614b915f673695dbb8658198eb
marinko malencic <marinko.malencic542@gmail.com>
2023-07-24 19:05:49 +0200

    chore: add 2 new icons

---
+++ src/assets/backward.js
@@ -0,0 +1,23 @@
+const Backward = ({ className }) => {
+  return (
+    // <?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+    <svg
+      // width="80px"
+      // height="80px"
+      viewBox="0 0 24 24"
+      fill="white"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M12 13.8L19 18V6L12 10.2M12 18L12 6L3 12L12 18Z"
+        stroke="gray"
+        strokeWidth=".7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Backward;
---
+++ src/assets/forward.js
@@ -0,0 +1,24 @@
+const Forward = ({ className }) => {
+  return (
+    // <?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+    <svg
+      // width="80px"
+      // height="80px"
+      viewBox="0 0 24 24"
+      fill="white"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path opacity="0.15" d="M11 18L11 6L20 12L11 18Z" fill="grey" />
+      <path
+        d="M12 10.2L5 6L5 18L12 13.8M12 6L12 18L21 12L12 6Z"
+        stroke="grey"
+        strokeWidth=".7"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default Forward;
