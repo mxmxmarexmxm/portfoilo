86b733d2da26a135716b11d97f6cf3b161c1535c
marinko malencic <marinko.malencic542@gmail.com>
2023-07-24 20:02:04 +0200

    feat: make carrousel length indicator

--- src/components/ProjectCarrousel.js
+++ src/components/ProjectCarrousel.js
@@ -6,10 +6,9 @@ import Backward from '../assets/icons/backward';
 
 const ProjectCarrousel = () => {
   const [project, setProject] = useState(0);
+  const projectLength = projectsData.length;
 
   const carrouselHandler = (direction) => {
-    let projectLength = projectsData.length;
-
     if (direction === 'forward') {
       setProject((current) => {
         if (project === projectLength - 1) {
@@ -28,17 +27,27 @@ const ProjectCarrousel = () => {
   };
 
   return (
-    <div className="flex items-center justify-between sm:px-4">
-      <button onClick={() => carrouselHandler('backward')}>
-        <Backward className="  cursor-pointer w-8 sm:w-24" />
-      </button>
+    <>
+      <div className="flex items-center justify-between sm:px-4">
+        <button onClick={() => carrouselHandler('backward')}>
+          <Backward className="  cursor-pointer w-8 sm:w-24" />
+        </button>
 
-      <Project {...projectsData[project]} />
+        <Project {...projectsData[project]} />
 
-      <button onClick={() => carrouselHandler('forward')}>
-        <Forward className=" cursor-pointer w-8 sm:w-24" />
-      </button>
-    </div>
+        <button onClick={() => carrouselHandler('forward')}>
+          <Forward className=" cursor-pointer w-8 sm:w-24" />
+        </button>
+      </div>
+
+      <div className="bg-gray-500 w-64 h-8 self-center rounded-2xl flex justify-start overflow-hidden border-2 border-gray-700">
+        <div
+          className={`w-[${
+            ((project + 1) / projectLength) * 100
+          }%] bg-gray-600 h-full border-r-2 border-gray-700`}
+        ></div>
+      </div>
+    </>
   );
 };
 
