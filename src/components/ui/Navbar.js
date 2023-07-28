2922c99c9a78e49eecbaa45d135df9ae03d2de2a
marinko malencic <marinko.malencic542@gmail.com>
2023-07-28 19:43:35 +0200

    feat: add settings to the navbar

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import Settings from '../../assets/icons/settings';
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const { openSettings } = props;
   return (
     <nav className="smooth-scroll">
       <ul className="flex fixed top-0 right-0 gap-8 bg-black w-full justify-end px-8 py-2 sm:py-4 font-semibold sm:text-xl z-50">
@@ -12,6 +14,9 @@ const Navbar = () => {
         </li>
         <li className="cursor-pointer">
           <a href="#contact">Contact</a>
+        </li>
+        <li className="cursor-pointer" onClick={openSettings}>
+          <Settings className="cursor-pointer w-8" />
         </li>
       </ul>
     </nav>
