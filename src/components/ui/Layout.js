1e4c3ec907966bcc5ef55dc9f1d2746eb5d330f5
marinko malencic <marinko.malencic542@gmail.com>
2023-07-31 21:43:02 +0200

    feat: stop animation in layout

--- src/components/ui/Layout.js
+++ src/components/ui/Layout.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import CanvasComponent from './CanvasComponent';
 
 const Layout = (props) => {
+  const { settings } = props;
   return (
     <div className="w-full min-h-full text-white sm:p-4 p-1 flex justify-center">
-      <div className="absolute top-0 left-0 w-full h-full z-[-1]">
-        <CanvasComponent />
+      <div
+        className={`absolute top-0 left-0 w-full h-full z-[-10} ${
+          settings.animation ? 'opacity-100' : 'opacity-0 pointer-events-none'
+        } transition-opacity duration-300`}
+      >
+        <CanvasComponent settings={settings} />
       </div>
       {props.children}
     </div>
