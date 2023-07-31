374b60fef9daf98414da779fc6407d1c192fe88b
marinko malencic <marinko.malencic542@gmail.com>
2023-07-31 21:43:30 +0200

    feat: menage settings in the app component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
+import { useState } from 'react';
 import Navbar from './components/ui/Navbar';
 import Layout from './components/ui/layout';
 import Home from './pages/Home';
+import Modal from './components/ui/Modal';
 
 function App() {
+  const [openModal, setOpenModal] = useState(false);
+  const [settings, setSettings] = useState({
+    fps: 23,
+    color: '#292727',
+    charset: 'M',
+    size: 12,
+    animation: true,
+  });
+
   return (
-    <Layout>
-      <Navbar />
-      <Home />
-    </Layout>
+    <>
+      {openModal && (
+        <Modal
+          setIsOpen={setOpenModal}
+          setSettings={setSettings}
+          settings={settings}
+        />
+      )}
+      <Layout settings={settings}>
+        <Navbar openSettings={() => setOpenModal(true)} />
+        <Home />
+      </Layout>
+    </>
   );
 }
 
