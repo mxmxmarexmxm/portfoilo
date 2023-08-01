69cc56305f865055130f1c7e8504ad636fee0fe1
marinko malencic <marinko.malencic542@gmail.com>
2023-08-01 17:52:10 +0200

    style: adopted settings modal for phone preview

--- src/components/ui/Modal.js
+++ src/components/ui/Modal.js
@@ -27,11 +27,13 @@ const Modal = (props) => {
 
   return (
     <div className="text-white fixed h-full w-full z-50 flex items-center justify-center bg-[rgba(0,0,0,0.9)] ">
-      <div className="bg-gray-800 opacity-100 h-[550px] aspect-square rounded-2xl flex flex-col py-8 px-12">
-        <h2 className="font-bold text-3xl mb-8 text-center">Settings</h2>
+      <div className="bg-gray-800 opacity-100 sm:w-[550px] w-[95%] aspect-square rounded-2xl flex flex-col sm:py-8 sm:px-12 p-4">
+        <h2 className="font-bold text-2xl sm:text-3xl mb-8 text-center">
+          Settings
+        </h2>
         <form
           onSubmit={handleSubmit}
-          className="flex flex-col justify-between flex-1 gap-4 text-xl font-bold"
+          className="flex flex-col justify-between flex-1 gap-4 sm:text-xl font-bold"
         >
           <div className="flex justify-between">
             <label htmlFor="animation">Background animation</label>
@@ -78,9 +80,9 @@ const Modal = (props) => {
           </div>
           <div className="flex justify-between items-center">
             <label htmlFor="color">Color</label>
-            <div className="rounded-full border-2 border-white overflow-hidden w-10 h-10">
+            <div className="rounded-full border-2 border-white flex items-center justify-center overflow-hidden w-10 h-10 ">
               <input
-                className="text-black cursor-pointer w-[200%] h-[200%]"
+                className="text-black cursor-pointer w-[200%] h-[200%] aspect-square"
                 type="color"
                 name="color"
                 value={formState.color}
@@ -89,7 +91,7 @@ const Modal = (props) => {
               />
             </div>
           </div>
-          <div className="flex gap-4 justify-center h-12">
+          <div className="flex gap-4 justify-center h-12 mt-6">
             <button
               type="button"
               className="w-32 bg-gray-900 rounded-xl"
