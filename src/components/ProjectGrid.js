20f39dc3404c68374982a31ab666bd44f14424ab
marinko malencic <marinko.malencic542@gmail.com>
2023-08-02 21:12:08 +0200

    style: increse gap for phone preview

--- src/components/ProjectGrid.js
+++ src/components/ProjectGrid.js
@@ -4,7 +4,7 @@ import Project from './Project';
 
 const ProjectGrid = () => {
   return (
-    <div className="grid sm:grid-cols-[repeat(auto-fill,minmax(700px,1fr))] sm:gap-y-0">
+    <div className="grid sm:grid-cols-[repeat(auto-fill,minmax(700px,1fr))] sm:gap-y-0 gap-y-16">
       {projects.map((project) => (
         <div className="sm:p-24">
           <Project {...project} />
