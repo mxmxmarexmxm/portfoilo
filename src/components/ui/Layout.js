f19be4a901cf07ee86e3f65c300881ad69bc86da
marinko malencic <marinko.malencic542@gmail.com>
2023-08-02 20:46:53 +0200

    chore: stop animation in component instead in layout

--- src/components/ui/Layout.js
+++ src/components/ui/Layout.js
@@ -1,16 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CanvasComponent from './CanvasComponent';
 
 const Layout = (props) => {
-  const { settings } = props;
   return (
     <div className="w-full min-h-full text-white sm:p-4 p-1 flex justify-center">
       <div
-        className={`absolute top-0 left-0 w-full h-full z-[-10} ${
-          settings.animation ? 'opacity-100' : 'opacity-0 pointer-events-none'
+        className={`absolute top-0 left-0 w-full h-full z-[-10} opacity-100
         } transition-opacity duration-300`}
       >
-        <CanvasComponent settings={settings} />
+        <CanvasComponent />
       </div>
       {props.children}
     </div>
