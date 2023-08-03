ff06ee6a8f1a494326f36c3d5e31f5062c2dcaf5
marinko malencic <marinko.malencic542@gmail.com>
2023-08-03 20:56:26 +0200

    chore: make icons to accept color prop

--- src/assets/icons/forward.js
+++ src/assets/icons/forward.js
@@ -1,11 +1,11 @@
-const Forward = ({ className }) => {
+const Forward = ({ className, color }) => {
   return (
     // <?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
     <svg
       // width="80px"
       // height="80px"
       viewBox="0 0 24 24"
-      fill="white"
+      fill={color}
       xmlns="http://www.w3.org/2000/svg"
       className={className}
     >
