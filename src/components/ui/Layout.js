56ee4427813b8509a59ae19a220da5ea4bcd39a6
marinko malencic <marinko.malencic542@gmail.com>
2023-08-03 19:54:51 +0200

    chore: implement background color settings in layout

--- src/components/ui/Layout.js
+++ src/components/ui/Layout.js
@@ -8,7 +8,10 @@ const Layout = (props) => {
   return (
     <div
       className="w-full min-h-full  sm:p-4 p-1 flex justify-center"
-      style={{ color: settings.primaryTextColor }}
+      style={{
+        color: settings.primaryTextColor,
+        backgroundColor: !settings.animation ? settings.backgroundColor : '',
+      }}
     >
       <div
         className={`absolute top-0 left-0 w-full h-full z-[-10] opacity-100
