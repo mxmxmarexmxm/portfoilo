7f2d3a21c88482c15a1cb78b68f5b125ab138c88
marinko malencic <marinko.malencic542@gmail.com>
2023-08-08 19:31:10 +0200

    chore: add two icons

---
+++ src/assets/icons/done.js
@@ -0,0 +1,40 @@
+const DoneIcon = ({ className, color }) => {
+  return (
+    <svg
+      fill={color}
+      viewBox="0 0 20 20"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      // xmlns:xlink="http://www.w3.org/1999/xlink"
+      className={className}
+    >
+      <title>
+        interface / 8 - interface, radio button, checked, checkmark, done, to
+        do, radio icon
+      </title>
+      <g
+        id="Free-Icons"
+        stroke="none"
+        strokeWidth="1"
+        fill="none"
+        fillRule="evenodd"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <g
+          transform="translate(-1265.000000, -526.000000)"
+          id="Group"
+          stroke={color}
+          stroke-width="2"
+        >
+          <g transform="translate(1263.000000, 524.000000)" id="Shape">
+            <path d="M12,3 C16.9705627,3 21,7.02943725 21,12 C21,16.9705627 16.9705627,21 12,21 C7.02943725,21 3,16.9705627 3,12 C3,7.02943725 7.02943725,3 12,3 Z"></path>
+            <polyline points="7.71428571 11.6223394 11.2436971 15 16.2857143 9"></polyline>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default DoneIcon;
