66b4388a5b2ba63de276a23bcb0a5529631ce65d
marinko malencic <marinko.malencic542@gmail.com>
2023-08-14 18:32:49 +0200

    chore: update icon props

--- src/assets/icons/info.js
+++ src/assets/icons/info.js
@@ -16,9 +16,9 @@ const InfoIcon = ({ className, color }) => {
           id="Vector"
           d="M12 11V16M12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21ZM12.0498 8V8.1L11.9502 8.1002V8H12.0498Z"
           stroke={color}
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </g>
     </svg>
