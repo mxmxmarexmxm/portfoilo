4fbeab71cc818ba9ab5bfa0f3b7c5ebe757f75fa
marinko malencic <marinko.malencic542@gmail.com>
2023-08-29 19:54:45 +0200

    refactor: refactor about-me page

--- src/pages/about-me.js
+++ src/pages/about-me.js
@@ -1,13 +1,35 @@
-const AboutMe = () => {
-  return (
-    <div className="flex items-center h-screen px-16">
-      <h1 className="text-5xl font-black">
-        Hi, I'm Marinko
-        <br />
-        Frontend Developer
-      </h1>
-    </div>
-  )
-};
+import { useContext } from 'react';
+import { SettingsContext } from '../context/SettingsContext';
 
+const AboutMe = () => {
+  const { settings } = useContext(SettingsContext);
+  return (
+    <div className="flex items-center justify-start h-screen px-16 text-center w-full font-elite">
+      <div className="w-full flex flex-col gap-8">
+        <div className="text-[25rem] font-semibold flex leading-[18rem] justify-center">
+          <div className="bounce-in-m-1">
+            <span>
+              M<span className="text-8xl">x</span>
+            </span>
+          </div>
+          <div className="bounce-in-m-2">
+            <span>
+              M<span className="text-8xl">x</span>
+            </span>
+          </div>
+        </div>
+        <div className="flex justify-center items-center w-full text-7xl font-semibold text-center flex-col">
+          <h1>
+            Hi, I'm{' '}
+            <span style={{ color: settings.secondaryTextColor }}>Marinko.</span>
+            <br />
+          </h1>
+          <div className="typewriter">
+            <span>I'm a frontend web developer.</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default AboutMe;
