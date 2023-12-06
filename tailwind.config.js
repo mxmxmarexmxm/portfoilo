db4ce93e5f00e7287c435291a882e46badc198a3
marinko malencic <marinko.malencic542@gmail.com>
2023-12-06 19:46:31 +0100

    chore: add custom color to the tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx}",
-  ],  theme: {
-    extend: {},
+  content: ['./src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      backgroundColor: {
+        gray1: '#2e2e2e',
+      },
+    },
   },
   plugins: [],
-}
-
+};
