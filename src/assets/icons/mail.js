d9f35b22595d7bce6ae0587593c7f9d9c63f217e
marinko malencic <marinko.malencic542@gmail.com>
2024-01-09 20:37:12 +0100

    chore: update icon props

--- src/assets/icons/mail.js
+++ src/assets/icons/mail.js
@@ -8,11 +8,11 @@ const MailIcon = ({ className, color }) => {
       className={className}
       fill={color}
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
       <g
         id="SVGRepo_tracerCarrier"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       ></g>
       <g id="SVGRepo_iconCarrier">
         <circle cx="256" cy="256" r="256"></circle>
